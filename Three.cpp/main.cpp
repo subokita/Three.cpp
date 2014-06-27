186ac98fe8e42ab46e27e7b0f6e41eded16a9d17
subokita <saburookita@Saburos-MacBook-Air.local>
2014-06-27 07:19:42 +0200

    Updated main

--- Three.cpp/main.cpp
+++ Three.cpp/main.cpp
@@ -13,48 +13,6 @@
 
 using namespace std;
 
-
-void test2(glm::vec2& vec) {
-    vec = glm::vec2(1.0, 1.0);
-}
-
-void test1(glm::vec2 vec) {
-    vec = glm::vec2(1.0, 1.0);
-}
-
 int main(int argc, char **argv) {
-
-    auto start = chrono::high_resolution_clock::now();
-    
-    for( int i = 0; i < 100000; i++ ) {
-        glm::vec2 vector(0.0, 5.0);
-        test1( vector );
-    }
-    
-    auto end = chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count() << " ns" << std::endl;
-    
-    
-    start = chrono::high_resolution_clock::now();
-    
-    for( int i = 0; i < 100000; i++ ) {
-        test1( glm::vec2(0.0, 5.0) );
-    }
-    
-    end = chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count() << " ns" << std::endl;
-    
-
-    
-    
-    start = chrono::high_resolution_clock::now();
-    
-    for( int i = 0; i < 100000; i++ ) {
-        glm::vec2 vector(0.0, 5.0);
-        test2( vector );
-    }
-    
-    end = chrono::high_resolution_clock::now();
-    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end-start).count() << " ns" << std::endl;
-        return 0;
+    return 0;
 }
